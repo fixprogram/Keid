9d20a721559d200a7613e01076e448b3b878b90e
Denis <fixprogram@mail.ru>
2023-04-12 07:35:46 +0300

    revert this commit

--- src/templates/TaskPage/index.tsx
+++ src/templates/TaskPage/index.tsx
@@ -1,6 +1,5 @@
-import { useAppSelector } from "@/shared/lib/hooks/useAppSelector";
 import Layout from "@/widgets/Layout";
-import AddSubtaskPopup from "./components/AddSubtaskPopup";
+import AddSubtaskPopup from "@/widgets/SubtaskList/components/AddSubtaskPopup";
 import TaskBody from "./components/TaskBody";
 import TaskHeader from "./components/TaskHeader";
 import TaskSettings from "./components/TaskSettings";
