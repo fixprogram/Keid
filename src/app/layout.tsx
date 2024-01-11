b113a64511d67c150bb80d8229b33184558971c9
Denis <fixprogram@mail.ru>
2024-01-11 18:37:54 +0200

    added pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,23 @@ import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import Providers from "@/utils/providers";
 import { MAX_WIDTH } from "@/shared/config/consts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Keid",
+  description: "Keid",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "/public/apple-touch-icon.png" },
+    { rel: "icon", url: "/public/icon-192.png" },
+  ],
+};
 
 const inter = Inter({
   weight: ["400", "500", "600", "700"],
@@ -24,6 +41,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
     <html lang="en">
       <head>
         <title>Keid</title>
+        <link rel="manifest" href="/public/manifest.json" />
       </head>
       <body
         className={`${inter.variable} ${poppins.variable} font-sans`}
